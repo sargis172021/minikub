cce0eeda886f3d315cb790eaea624b49c6764f87
sargis172021 <sargis.hovhannisyan1720@gmail.com>
2025-03-17 11:04:43 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
